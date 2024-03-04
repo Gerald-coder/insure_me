f8ac2c7448ae63d4b2a75d726417b97f5d5bff83
Oduigwe Gerald <paulsongerald00@gmail.com>
2024-03-05 00:16:59 +0100

    bootstrapped site with tailwind and react

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div>
+        <h1 className="border-2 border-red-400">hello kizzy</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
