f3f3874428b58b9cd0ba126ca415422b7e5297f0
solisoma <solisomamadubueze@gmail.com>
2024-03-06 12:34:54 +0100

    completed insurance plan

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,20 @@
 import Box from "./components/Box";
 import Hero from "./components/Hero";
 import Nav from "./components/Nav";
+import { BsHeartPulse } from "react-icons/bs";
+import PricingPlan from "./components/PricingPlan";
 
 export default function App() {
   return (
     <>
-      <div className="h-[10vh]  border-b-2 border-gray-400 mb-4">
+      <div className="h-[10vh]  border-b-2 border-gray-400">
         <Nav />
       </div>
       <div className="h-[87vh] overflow-y-scroll remove-scrollbar">
-        {/* <Hero /> */}
-        <Box />
+        <Hero />
+        <div className="flex flex-col items-center my-4">
+          <PricingPlan />
+        </div>
       </div>
     </>
   );
--- src/components/Box.jsx
+++ src/components/Box.jsx
@@ -1,5 +1,18 @@
 import React from "react";
 
-export default function Box() {
-  return <div className="border-2 border-red-300 w-[38vw] md:w-[18vw]"></div>;
+export default function Box({ children, heading, desc, buttonColor }) {
+  return (
+    <div className="flex justify-center w-[40vw] mx-2 my-2 h-[50vh] border-2 border-gray-300 rounded-lg md:w-[16vw]">
+      <div className="flex flex-col my-3 px-2 items-center justify-between">
+        <h2 className="font-normal text-base md:text-xl">{heading}</h2>
+        <div className="py-1">{children}</div>
+        <p className="text-center text-gray-800 text-sm font-normal">{desc}</p>
+        <button
+          className={`py-2 px-4 ${buttonColor} text-white font-semibold text-xm rounded-lg`}
+        >
+          Choose Plan
+        </button>
+      </div>
+    </div>
+  );
 }
