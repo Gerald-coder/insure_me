fb7d335cd9d2fbb0776e7d3711c048215273ccb0
solisoma <solisomamadubueze@gmail.com>
2024-03-16 19:54:53 +0100

    resolve issue

--- src/App.jsx
+++ src/App.jsx
@@ -10,13 +10,13 @@ import Footer from "./components/Footer";
 
 export default function App() {
   return (
-    <div className="h-screen">
+    <div className="h-screen overflow-hidden">
       <div className="h-[14%] py-3 border-b-2 border-gray-400 md:h-[15%]">
         <Nav />
       </div>
       <div className="h-[86%] overflow-y-scroll remove-scrollbar !scroll-smooth md:h-[85%]">
         <Hero />
-        <div className="flex flex-col items-center mt-4">
+        <div className="flex flex-col items-center">
           <Service />
           <LearnMore />
           <WhatWeDo />
