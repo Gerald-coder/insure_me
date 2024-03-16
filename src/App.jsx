876a977ad01688794201f625bac27b0e1bd7c1c0
Oduigwe Gerald <paulsongerald00@gmail.com>
2024-03-16 22:43:31 +0100

    pushed

--- src/App.jsx
+++ src/App.jsx
@@ -11,8 +11,8 @@ import Footer from "./components/Footer";
 export default function App() {
   return (
     <div className="h-screen overflow-hidden">
-      <div className="h-[14%] py-3 border-b-2 border-gray-400 md:h-[15%]">
-        <Nav />
+      <div className="relative h-[14%] py-3 border-b-2 border-gray-400 md:h-[15%]">
+        <Nav className="absolute top-0 left-0 w-full" />
       </div>
       <div className="h-[86%] overflow-y-scroll remove-scrollbar !scroll-smooth md:h-[85%]">
         <Hero />
